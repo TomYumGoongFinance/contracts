8fc8562757e063017498e0f816ac65426c38ab3e
tomyumchef <tomyumchef@gmail.com>
2021-07-13 17:33:04 +0700

    Add tests for GoongeryHelper

--- test/goongeryHelper.test.js
+++ test/goongeryHelper.test.js
@@ -11,10 +11,45 @@ describe("GoongeryHelper", async function () {
   })
 
   describe("calculateGoongeryNumberId", async function () {
-    it("should generate unique ids for every possible numbers", async function () {})
+    it("should generate unique ids for every possible numbers", async function () {
+      const maxNumber = 15
+      const set = new Set()
+      for (let i = 0; i < maxNumber ** 3; i++) {
+        const first = parseInt(i / (maxNumber + 1) ** 2) % (maxNumber + 1)
+        const second = parseInt(i / (maxNumber + 1)) % (maxNumber + 1)
+        const third = i % (maxNumber + 1)
+        const id = await helper.calculateGoongeryNumberId([
+          first,
+          second,
+          third
+        ])
+        set.add(id)
+      }
+
+      expect(Array.from(set).length).to.be.eq(maxNumber ** 3)
+    })
   })
 
   describe("getLeastPermutableNumber", async function () {
-    it("should return least possible numbers", async function () {})
+    it("should return least possible numbers", async function () {
+      expect(await helper.getLeastPermutableNumber([3, 2, 1])).to.be.eql([
+        1, 2, 3
+      ])
+      expect(await helper.getLeastPermutableNumber([3, 1, 2])).to.be.eql([
+        1, 2, 3
+      ])
+      expect(await helper.getLeastPermutableNumber([2, 3, 1])).to.be.eql([
+        1, 2, 3
+      ])
+      expect(await helper.getLeastPermutableNumber([2, 1, 3])).to.be.eql([
+        1, 2, 3
+      ])
+      expect(await helper.getLeastPermutableNumber([1, 2, 3])).to.be.eql([
+        1, 2, 3
+      ])
+      expect(await helper.getLeastPermutableNumber([1, 3, 2])).to.be.eql([
+        1, 2, 3
+      ])
+    })
   })
 })
