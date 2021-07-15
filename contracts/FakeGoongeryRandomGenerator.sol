fb712e56fdc0a62d36782d8721d0065f909de363
tomyumchef <tomyumchef@gmail.com>
2021-07-15 13:14:19 +0700

    Add FakeGoongeryRandomGenerator for test

---
+++ contracts/FakeGoongeryRandomGenerator.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IGoongery.sol";
+import "./libs/IBEP20.sol";
+
+contract FakeGoongeryRandomGenerator is VRFConsumerBase, Ownable {
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    address internal requester;
+    address public link;
+    uint256 public randomResult;
+    IGoongery public goongery;
+    uint256 public roundNumber;
+
+    modifier onlyGoongery() {
+        require(msg.sender == address(goongery), "Caller must be Goongery");
+        _;
+    }
+
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        address _goongery,
+        bytes32 _keyHash,
+        uint256 _fee
+    ) public VRFConsumerBase(_vrfCoordinator, _linkToken) {
+        goongery = IGoongery(_goongery);
+        fee = _fee;
+        link = _linkToken;
+        keyHash = _keyHash;
+    }
+
+    function getRandomNumber(uint256 _roundNumber)
+        external
+        onlyGoongery
+        returns (uint256)
+    {
+        require(keyHash != bytes32(0), "Must have valid key hash");
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        requester = msg.sender;
+        roundNumber = _roundNumber;
+        return roundNumber;
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        IGoongery(requester).drawWinningNumbersCallback(
+            roundNumber,
+            requestId,
+            randomness
+        );
+        randomResult = randomness;
+    }
+
+    function fakeFulfillRandomness(bytes32 requestId, uint256 randomness)
+        external
+    {
+        IGoongery(requester).drawWinningNumbersCallback(
+            roundNumber,
+            requestId,
+            randomness
+        );
+        randomResult = randomness;
+    }
+
+    function withdrawLink(uint256 amount) public onlyOwner {
+        IBEP20(link).transfer(msg.sender, amount);
+    }
+}
